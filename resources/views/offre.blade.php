5cc33bea0885ddd6903d02ce660c5cbe32670aa7
kamal ait omar <aitomarkamal@gmail.com>
2021-05-11 14:14:50 +0000

    small changes in offre page

--- resources/views/offre.blade.php
+++ resources/views/offre.blade.php
@@ -2,27 +2,26 @@
 
 @section('content')
 <div class="container">
-    <div class="bg-white p-2 m-2">
+    <div class="my-5 p-2 m-2">
         <div class="row">
             <div class="col-3 offset-1">
                 <img src="../assets/images/logo-placeholder.png" class="mx-auto col-12">
                 <div class="my-3">
-                    <p><span class="text-info">Tele : </span>0622222222</p>
-                    <p><span class="text-info">Email : </span>test@gmail.com</p>
-                    <p><span class="text-info">site web : </span>www.test.com</p>
-                    <p><span class="text-info">Adresse : </span>Synergie Media, Immeuble Borj Agadir, Avenue Kadi Ayad, Agadir</p>
+                    <p>0622222222</p>
+                    <p>test@gmail.com</p>
+                    <p>www.test.com</p>
+                    <p>Synergie Media, Immeuble Borj Agadir, Avenue Kadi Ayad, Agadir</p>
                 </div>
             </div>
             <div class="col-8 m-auto">
-                <p class="text-info mb-0">Nom :</p>
-                <h4 class="mb-3">{{$offre->nom_offre}}</h4>
-                <p class="text-info mb-0">Objet :</p>
+                <h1 class="mb-3">{{$offre->nom_offre}}</h1>
+                <h4 class="text-info mb-0">Objet :</h4>
                 <p class="mb-3">{{$offre->objet}}</p>
                 @if ($offre->condition != null)
-                    <p class="text-info mb-0">Condition :</p>
+                    <h4 class="text-info mb-0">Condition :</h4>
                     <p class="mb-3">{{$offre->condition}}</p>
                 @endif 
-                <p class="text-info mb-0">Description :</p>
+                <h4 class="text-info mb-0">Description :</h4>
                 <p class="mb-3">{{$offre->description}}</p>
 
                 <div class="row">
