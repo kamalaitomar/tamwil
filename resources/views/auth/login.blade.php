2dc3f0aa2c235242a8b809e21dfeb6c1ffd45318
kamal ait omar <aitomarkamal@gmail.com>
2021-05-25 10:34:49 +0100

    login page done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,39 +12,62 @@
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                     <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image pt-3">
+                            <img src="/admin/img/login.png" alt="login image" class="col-12">
+                        </div>
                         <div class="col-lg-6">
                             <div class="p-5">
                                 <div class="text-center">
                                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                 </div>
-                                <form class="user">
+                                <form class="user"  method="POST" action="{{ route('login') }}">
+                                    @csrf
                                     <div class="form-group">
-                                        <input type="email" class="form-control form-control-user"
-                                            id="exampleInputEmail" aria-describedby="emailHelp"
-                                            placeholder="Enter Email Address...">
+                                        <input 
+                                            type="email" 
+                                            class="form-control form-control-user @error('email') is-invalid @enderror"
+                                            id="email" 
+                                            name="email"
+                                            value="{{ old('email') }}"
+                                            autocomplete="email" 
+                                            autofocus
+                                            required 
+                                            aria-describedby="emailHelp"
+                                            placeholder="{{ __('E-Mail Address') }}">
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control form-control-user"
-                                            id="exampleInputPassword" placeholder="Password">
+                                        <input 
+                                            id="password" 
+                                            type="password" 
+                                            class="form-control form-control-user @error('password') is-invalid @enderror"
+                                            name="password"
+                                            placeholder="{{ __('Password') }}"
+                                            autocomplete="current-password"
+                                            required >
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                     </div>
                                     <div class="form-group">
                                         <div class="custom-control custom-checkbox small">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
-                                            <label class="custom-control-label" for="customCheck">Remember
-                                                Me</label>
+                                            <input type="checkbox" class="custom-control-input" id="customCheck" {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="custom-control-label" for="customCheck">{{ __('Remember Me') }}</label>
                                         </div>
                                     </div>
-                                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                        Login
-                                    </a>
-                                    <hr>
-                                    <a href="index.html" class="btn btn-google btn-user btn-block">
-                                        <i class="fab fa-google fa-fw"></i> Login with Google
-                                    </a>
-                                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                    </a>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">{{ __('Login') }}</button>
+
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                            {{ __('Forgot Your Password?') }}
+                                        </a>
+                                    @endif
                                 </form>
                                 
                             </div>
@@ -58,7 +81,7 @@
     </div>
 
 </div>-
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -126,5 +149,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -33,19 +33,19 @@
 	    <div class="container">
 
             {{-- logo  --}}
-	        {{-- <a class="navbar-brand" href="{{ route('home', app()->getLocale()) }}">TAMWIL</a> --}}
+	        <a class="navbar-brand" href="{{ url('home') }}">TAMWIL</a>
 
             {{-- nav links  --}}
 	        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	            <span class="oi oi-menu"></span> Menu
 	        </button>
-	        <div class="collapse navbar-collapse" id="ftco-nav">
+	        {{--<div class="collapse navbar-collapse" id="ftco-nav">
                 <ul class="navbar-nav m-auto">
-                    {{-- <li class="nav-item"><a href="{{ route('home', app()->getLocale()) }} " class="nav-link">{{__('tamwil.home')}}</a></li>
+                     <li class="nav-item"><a href="{{ route('home', app()->getLocale()) }} " class="nav-link">{{__('tamwil.home')}}</a></li>
                     <li class="nav-item"><a href="{{ route('financement', app()->getLocale()) }}" class="nav-link">{{__('tamwil.funding')}}</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">{{__('tamwil.about')}}</a></li>
                     <li class="nav-item"><a href="#" class="nav-link">{{__('tamwil.organizations')}}</a></li>
-                    <li class="nav-item"><a href="{{ route('adminn', app()->getLocale()) }}" class="nav-link">{{__('tamwil.admin')}}</a></li> --}}
+                    <li class="nav-item"><a href="{{ route('adminn', app()->getLocale()) }}" class="nav-link">{{__('tamwil.admin')}}</a></li> 
                 </ul>
 	        </div>
             <div class="dropdown">
@@ -54,17 +54,17 @@
                 </button>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                     @if (Route::currentRouteName() == "offre")
-                        {{-- <a class="dropdown-item" href="{{ route(Route::currentRouteName(), ['locale'=>'ar', 'id'=>$id]) }}">{{__('tamwil.arabic') }}</a>
+                        <a class="dropdown-item" href="{{ route(Route::currentRouteName(), ['locale'=>'ar', 'id'=>$id]) }}">{{__('tamwil.arabic') }}</a>
                         <a class="dropdown-item" href="{{ route(Route::currentRouteName(), ['locale'=>'fr', 'id'=>$id]) }}">{{__('tamwil.french') }}</a>
-                        <a class="dropdown-item" href="{{ route(Route::currentRouteName(), ['locale'=>'en', 'id'=>$id]) }}">{{__('tamwil.english') }}</a> --}}
+                        <a class="dropdown-item" href="{{ route(Route::currentRouteName(), ['locale'=>'en', 'id'=>$id]) }}">{{__('tamwil.english') }}</a> 
                     @else
                     {{-- <a class="dropdown-item" href="{{ route(Route::currentRouteName(), 'ar') }}">{{__('tamwil.arabic') }}</a>
                     <a class="dropdown-item" href="{{ route(Route::currentRouteName(), 'fr') }}">{{__('tamwil.french') }}</a> 
-                    <a class="dropdown-item" href="{{ route(Route::currentRouteName(), 'en') }}">{{__('tamwil.english') }}</a>  --}}
+                    <a class="dropdown-item" href="{{ route(Route::currentRouteName(), 'en') }}">{{__('tamwil.english') }}</a> 
                     @endif
                   
                 </div>
-              </div>
+              </div> --}}
 	    </div>
     </nav>
     <!-- END nav -->
