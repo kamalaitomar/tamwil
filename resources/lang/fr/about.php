124566afd85e59ed82ff237a5866b4f1a5445a8b
FATIMA-ZAHRA <f.oumellaik@gmail.com>
2021-06-08 10:52:22 +0100

    add form in about page & translation

---
+++ resources/lang/fr/about.php
@@ -0,0 +1,15 @@
+<?php
+    return[
+        "Qui sommes nous ?"=>"Qui sommes nous ?",
+        "description"=>"Le CRI de Souss Massa est un établissement public jouissant de la personnalité morale et de l’autonomie financière. Il est dirigé par un Conseil d’Administration que préside le Wali de la Région et dans lequel siègent pratiquement tous présentants des principales administrations concernées : Président du Conseil régional, Agence Nationale de promotion de la PME, ANAPEC, OFPPT, CCG, Agence Urbaine, Président de la Chambre de Commerce et de l’Industrie, le représentant régional de l’organisation patronale la plus représentative, trois personnalités indépendantes choisies pour leur compétence …Ce qui est de nature à faciliter la prise de décision et assurer la fluidité de l’information sur la dynamique régionale et l’intelligence territoriale. ",
+        "Contactez-nous"=>"Contactez-nous",
+        "Email address"=>"Adresse e-mail",
+        "message"=>"message",
+        "Envoyer"=>"Envoyer",
+
+
+        
+        ]
+
+
+?>
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,27 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="my-5 p-2 m-2">
-        <h1 class="text-center m-2 p-2"> Qui sommes nous ?</h1>
-    
-          <div class="card  m-6">
-            <div class="card-body">
-              <p class="card-text">      Le Centre régional d’investissement (CRI) de Marrakech-Safi, la CGEM, Emerging business factory et la Near east Foundation ont mis en commun leurs ressources et expertise pour le développement de la plateforme Tamwil.<br>
-    
-                Cette plateforme se veut comme le carrefour des moyens et sources de financement proposés par les acteurs publics et privés.<br>
-                
-                Selon le communiqué du CRI, la plateforme Tamwil ne se limite pas au simple recensement des financements mais propose également un moteur de recherche régional. Une fois les caractéristiques des projets saisies, ce dernier génère une liste de solutions de financement accessibles à ces projets, en tenant compte de leur classification suivant certains critères tels que le coût, le risque et l’effort.<br>
-                
-                Pour le CRI de Marrakech-Safi, quant à lui, l’objectif est de doter le tissu économique régional d’un outil technologique permettant la simplification de leurs démarches administratives.
-                <br>
-                VOTRE PLATEFORME CRI EN LIGNE<br>
-                
-                https://www.cri-invest.ma/
-               </p>
+
+<div class="hero-wrap py-5" :style="{ backgroundImage: 'url(/assets/images/bg1.jpg)' }" data-stellar-background-ratio="0.5">
+
+
+    <div class="container ">
+        <div class="p-2">
+            <h1 class="text-center m-2 pb-4 "> {{__('about.Qui sommes nous ?')}} </h1>
+        
+            <div class="card  m-6">
+                <div class="card-body">
+                    <p class="card-text">
+                        {{__('about.description')}}
+                    </p>
+                </div>
+            </div> 
+            <div class="  justify-content-center align-items-center mt-5 col-8  ">
+                <h1 class="text-center m-2 pb-4 "> {{__('about.Contactez-nous')}} </h1>
+
+                <div class="my-3">
+                    <label for="exampleFormControlInput1" class="form-label"> {{__('about.Email address')}}</label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label"> {{__('about.message')}}</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <button type="submit" class="btn btn-outline-primary">{{__('about.Envoyer')}}</button>
             </div>
-          </div> 
+        </div>
     </div>
-</div>
-  
+</div>    
+
 @endsection
